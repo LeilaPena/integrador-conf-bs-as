727be9c5e4636847ed802000396ef566db3cd7f9
Leila Peña <leilajulieta1999@gmail.com>
2022-10-18 02:45:39 -0300

    Di funcionalidad a boton borrar

--- main.js
+++ main.js
@@ -4,6 +4,15 @@ const seccionConferencia = document.getElementById('seccion-conferencia');
 const seccionComprarTickets = document.getElementById('seccion-comprar-tickets');
 const botonConferencia = document.getElementById('boton-conferencia');
 const botonComprarTickets = document.getElementById('boton-comprar-tickets');
+const inputTicketsNombre = document.getElementById('input-tickets-nombre');
+const inputTicketsApellido = document.getElementById('input-tickets-apellido');
+const inputTicketsEmail = document.getElementById('input-tickets-email');
+const inputTicketsCantidad = document.getElementById('input-tickets-cantidad');
+const selectTicketsCategoria = document.getElementById('select-tickets-categoria');
+const botonTicketsBorrar = document.getElementById('boton-tickets-borrar');
+const botonTicketsResumen = document.getElementById('boton-tickets-resumen');
+
+// Botones para ver cada sección
 
 botonConferencia.addEventListener('click',()=>{
     seccionComprarTickets.style.display='none';
@@ -16,4 +25,15 @@ botonComprarTickets.addEventListener('click',()=>{
     seccionComprarTickets.style.display='block';
     botonConferencia.classList.remove('active');
     botonComprarTickets.classList.add('color-verde');
+})
+
+// Funcionalidad boton borrar inputs
+
+botonTicketsBorrar.addEventListener('click', (e) =>{
+    e.preventDefault()
+    inputTicketsNombre.value = "";
+    inputTicketsApellido.value = "";
+    inputTicketsEmail.value = "";
+    inputTicketsCantidad.value = 0;
+    selectTicketsCategoria.value = "estudiante";
 })
